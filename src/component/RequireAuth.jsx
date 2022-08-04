c7c0c5b6048c2ec02a6b2606ba3f51d18abdc88c
piyush7827 <piyushshukla7193@gmail.com>
2022-08-04 22:17:50 +0530

    first commit

---
+++ src/component/RequireAuth.jsx
@@ -0,0 +1,23 @@
+import { useLocation, Navigate, Outlet } from "react-router-dom";
+import React from "react";
+const RequireAuth = ({ allowedRoles }) => {
+  const location = useLocation();
+
+  return (
+    // wrap this condition with status = logged in
+    localStorage.getItem("userTypes") === allowedRoles[0] ? ( // admin // customer // engineer
+      // placeholder for react component
+      <Outlet />
+    ) : localStorage.getItem("userTypes") ? (
+      <Navigate to="/unauthorized" state={{ from: location }} replace />
+    ) : (
+      <Navigate to="/" state={{ from: location }} replace />
+    )
+  );
+};
+
+// var x = false
+
+// x ? "Utkarshini Arora" : "Lorem ipsum"
+
+export default RequireAuth;
